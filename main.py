d6e1181dd7955ae855f7f61f209f4b401f26f438
Patipol Saengduean <patipol.saeng@gmail.com>
2024-04-29 16:47:19 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,56 @@
+''' training 
+'''
+import os, sys
+import numpy as np
+import yaml
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+import torch
+import shutil
+from copy import deepcopy
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '.')))
+np.random.seed(0)
+
+def main(cfg):
+    # creat folders # ! don't create a cfg yet
+    os.makedirs(os.path.join(cfg.output_dir, cfg.train.log_dir), exist_ok=True)
+    with open(os.path.join(cfg.output_dir, cfg.train.log_dir, 'full_config.yaml'), 'w') as f:
+        yaml.dump(cfg, f, default_flow_style=False)
+    shutil.copy(cfg.cfg_file, os.path.join(cfg.output_dir, 'config.yaml'))
+    
+    # cudnn related setting
+    cudnn.benchmark = True
+    torch.backends.cudnn.deterministic = False
+    torch.backends.cudnn.enabled = True
+
+    # start training
+    from decalib.deca import DECA # !
+    from lib.trainer import Trainer
+    cfg.rasterizer_type = 'pytorch3d'
+    
+    # torch.cuda.set_device(cfg.device_id)
+    device = torch.device(cfg.device)
+    deca = DECA(cfg, device = device)
+    trainer = Trainer(model=deca, config=cfg, device = device)
+
+    ## start train
+    if cfg.dataset.test_mode:
+        trainer.test_img(cfg.dataset.test_data)
+    else:
+        os.makedirs(os.path.join(cfg.output_dir, cfg.train.vis_dir), exist_ok=True)
+        os.makedirs(os.path.join(cfg.output_dir, cfg.train.val_vis_dir), exist_ok=True)
+        trainer.fit()
+
+if __name__ == '__main__':
+    from decalib.utils.config import parse_args
+    cfg = parse_args()
+    if cfg.cfg_file is not None:
+        exp_name = cfg.cfg_file.split('/')[-1].split('.')[0]
+        cfg.exp_name = exp_name
+    main(cfg)
+
+# run:
+# python main_train.py --cfg configs/release_version/deca_pretrain.yml 
+# asd a
